4e69405f6b200311b5fda3c6c00391233c69f0f4
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-19 19:57:44 +0000

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# ICS2O ⇢ Unit #6-02-B
+
+[![Mr Coxall's Super Linter](https://github.com/MTHS-ICD2O-1-2024/ICD2O-Unit-6-02-B-ain-jeong/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/MTHS-ICD2O-1-2024/ICD2O-Unit-6-02-B-ain-jeong/actions)
+
+This site can be found at: [https:/https://icd2o-1-teh.type-code.cloud/~ain.jeong/ICD2O-1/Unit-5/ICD2O-Unit-6-02-B-Ain-Jeong/](https:/https://icd2o-1-teh.type-code.cloud/~ain.jeong/ICD2O-1/Unit-5/ICD2O-Unit-6-02-B-Ain-Jeong/)
+---
+
+**NOTES ON INITIAL LOAD:**
+- remember to goto on the menu bar:
+  - File
+  - Go to Repository
+  - ⚙ Settings
+  - 🗔 Pages
+    - then from the "Deploy from a branch ▼" button, select "GitHub Actions"
---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Copyright (c) 2025 Ain Jeong All rights reserved
+//
+// Created by: Ain Jeong
+// Created on: Apr 2025
+// This file contains the JS functions for index.html
+
+/**
+ * This function check user's age and the type of movie they can watch
+ */
+// eslint-disable-next-line no-unused-vars
+function checkAge() {
+  // input
+  const userAge = parseFloat(document.getElementById('user-age').value)
+
+  // save
+  localStorage.setItem('savedAge', userAge)
+
+  // process
+  if (userAge >= 17) {
+    // output
+    document.getElementById('answer').innerHTML =
+      'You can watch R movies alone.'
+  } else if (userAge >= 13) {
+    // output
+    document.getElementById('answer').innerHTML =
+      'You can watch PG-13 movies alone.'
+  } else if (userAge >= 5) {
+    // output
+    document.getElementById('answer').innerHTML =
+      'You can watch G or PG movies alone.'
+  } else {
+    // output
+    document.getElementById('answer').innerHTML =
+      'You are too young for most of the movies.'
+  }
+}
+
+/**
+ * This function shows the saved age
+ */
+// eslint-disable-next-line no-unused-vars
+window.onload = function () {
+  const savedAge = localStorage.getItem('savedAge')
+
+  if (savedAge !== null) {
+    document.getElementById('user-age').value = savedAge
+  }
+}
